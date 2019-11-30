14a1f4a58d064bda85e12a5d8bc1970c3acb3f48
yukijina <yukijina@gmail.com>
2019-11-29 16:18:00 -0800

    loading spinner

---
+++ src/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { POINT_CONVERSION_COMPRESSED } from 'constants';
+
+const Spinner = (props) => {
+    return (
+        <div class="ui active dimmer">
+            <div class="ui big text loader">{props.message}</div>
+        </div>
+  
+    )
+}
+
+Spinner.defaultProps = {
+    message: "Loading..."
+}
+export default Spinner;
