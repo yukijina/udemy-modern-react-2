374fd3f6ab37012e4aa81902c91cc1756cd0ce19
yukijina <yukijina@gmail.com>
2020-01-15 16:08:27 -0800

    move useEffect to useLocation file

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import SeasonDisplay from './SeasonDisplay'
 import Spinner from './Spinner'
+import useLocation from './useLocation';
 
 const App = () => {
-    const [lat, setLat] = useState(null)
-    const [errorMessage, setErrorMessage] = useState('')
-
-    useEffect(() => {
-        window.navigator.geolocation.getCurrentPosition(
-            position => setLat(position.coords.latitude),
-            err => setErrorMessage(err.message)
-        )
-    },[]);
-
+    const [lat, errorMessage] = useLocation();
+    
     let content;
     if (errorMessage) {
         content = <div>Error: {errorMessage}</div>
